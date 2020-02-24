f21167b83a73b146749bf1f69caa1807686794e4
Nathan Holden <nathanrholden@gmail.com>
2020-02-23 19:12:22 -0500

    Problem 2 Completion

--- holden_problem2/src/SavingsAccount.java
+++ holden_problem2/src/SavingsAccount.java
@@ -1,4 +1,20 @@
 
 public class SavingsAccount {
-
+	
+	private static double annualInterestRate;
+	private double savingsBalance;
+	
+	public SavingsAccount(double savingsBalance) {
+		this.savingsBalance = savingsBalance;
+	}
+	
+	public void calculateMonthlyInterest() {
+		double monthlyInterestRate = annualInterestRate / 12;
+		savingsBalance = savingsBalance + (savingsBalance * monthlyInterestRate);
+		System.out.printf("%.2f\n", savingsBalance);
+	}
+	
+	public static void modifyInterestRate(double interestRate) {
+		annualInterestRate = interestRate;
+	}
 }
--- holden_problem2/src/SavingsAccountTest.java
+++ holden_problem2/src/SavingsAccountTest.java
@@ -2,8 +2,23 @@
 public class SavingsAccountTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		SavingsAccount saver1 = new SavingsAccount(2000.00);
+		SavingsAccount saver2 = new SavingsAccount(3000.00);
+		
+		SavingsAccount.modifyInterestRate(0.04);
+		
+		System.out.print("saver1 balance: ");
+		saver1.calculateMonthlyInterest();
+		System.out.print("saver2 balance: ");
+		saver2.calculateMonthlyInterest();
+		
+		SavingsAccount.modifyInterestRate(0.05);
+		System.out.println();
+		
+		System.out.print("saver1 balance: ");
+		saver1.calculateMonthlyInterest();
+		System.out.print("saver2 balance: ");
+		saver2.calculateMonthlyInterest();
 	}
 
 }
